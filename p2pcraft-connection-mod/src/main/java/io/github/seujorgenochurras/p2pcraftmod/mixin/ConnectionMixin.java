9fca44ba96f5164c7c9df65c44fa023a82854001
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-05-04 16:38:19 -0300

    feat: Automatically hosts server

--- p2pcraft-connection-mod/src/main/java/io/github/seujorgenochurras/p2pcraftmod/mixin/ConnectionMixin.java
+++ p2pcraft-connection-mod/src/main/java/io/github/seujorgenochurras/p2pcraftmod/mixin/ConnectionMixin.java
@@ -1,22 +1,58 @@
 package io.github.seujorgenochurras.p2pcraftmod.mixin;
 
+import io.github.seujorgenochurras.p2pcraftmod.api.P2pCraftApi;
+import io.github.seujorgenochurras.p2pcraftmod.api.model.P2pServer;
+import io.github.seujorgenochurras.p2pcraftmod.ngrok.NgrokHelper;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ConnectScreen;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ConnectScreen.class)
-public class ConnectionMixin {
+public abstract class ConnectionMixin {
 
+    @Unique
+    private final P2pCraftApi p2pApi = new P2pCraftApi();
 
-    @Inject(method = "connect(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;Z)V",
-        at = @At("HEAD"))
-    private static void connect(Screen screen, MinecraftClient client, ServerAddress address, ServerInfo info, boolean quickPlay, CallbackInfo ci) {
-        System.out.println("ajwdojawodawjdoa \n\n\n\n foi funciona");
+    @Shadow
+    private void setStatus(Text status) {
+    }
+
+    @Shadow
+    protected abstract void connect(MinecraftClient client, ServerAddress address, @Nullable ServerInfo info);
+
+    @Inject(method = "connect(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;)V",
+        at = @At(value = "HEAD"), cancellable = true)
+    private void connect(MinecraftClient client, ServerAddress serverAddress, ServerInfo serverInfo, CallbackInfo ci) {
+        String staticAddress = serverAddress.getAddress();
+
+        P2pServer p2pServer = p2pApi.findServer(staticAddress);
+
+        if (!p2pServer.exists()) return;
+
+        setStatus(Text.of("Entrando no servidor P2PCraft..."));
+
+        String realAddress = p2pServer.getRealAddress();
+
+        if (!p2pServer.isOnline()) {
+            realAddress = startP2pHost();
+            p2pApi.sendNewHost(p2pServer, realAddress);
+        }
+        serverAddress = ServerAddress.parse(realAddress);
+        ci.cancel();
+        connect(client, serverAddress, serverInfo);
+    }
+
+    @Unique
+    private static String startP2pHost() {
+        return NgrokHelper.openTunnel();
     }
 }
