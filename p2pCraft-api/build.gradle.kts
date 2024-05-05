566cec6ddbb9698f90cc584c297bd6cca9cf53ef
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-05-05 14:20:38 -0300

    chore: Add spring boot configuration

--- p2pCraft-api/build.gradle.kts
+++ p2pCraft-api/build.gradle.kts
@@ -1,19 +1,22 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "io.github.seujorgenochurras"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
