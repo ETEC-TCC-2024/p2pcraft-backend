0e622f351bc45957f380410aacb358b3ed11735e
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-05-11 16:37:22 -0300

    chore: Added auto-reload to Docker

--- p2pCraft-api/build.gradle.kts
+++ p2pCraft-api/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.data:spring-data-jpa:3.2.5")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
