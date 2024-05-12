5b8cfa512fe0ff86d99edad4d56ceb540c8baaac
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-05-12 13:38:19 -0300

    refactor: Added default router to client controller

--- p2pCraft-api/src/main/java/io/github/seujorgenochurras/p2pApi/api/controller/ClientController.java
+++ p2pCraft-api/src/main/java/io/github/seujorgenochurras/p2pApi/api/controller/ClientController.java
@@ -5,26 +5,24 @@ import io.github.seujorgenochurras.p2pApi.domain.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/client")
 public class ClientController {
 
     @Autowired
     private ClientService clientService;
 
-    @PostMapping("/client")
+    @PostMapping
     public ResponseEntity<Client> registerClient(@RequestBody Client client) {
         Client persistedClient = clientService.save(client);
         return new ResponseEntity<>(persistedClient, HttpStatus.CREATED);
     }
 
-    @GetMapping("/client")
+    @GetMapping
     public ResponseEntity<List<Client>> getClients() {
         List<Client> clients = clientService.getAllClients();
         return new ResponseEntity<>(clients, HttpStatus.OK);
