8732477afdb36539394953abc56438440ebddf0d
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-02-25 16:47:25 -0300

    feat(Docker): Add AOT compilation for build

--- p2pCraft-api/build.gradle.kts
+++ p2pCraft-api/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.graalvm.buildtools.native") version "0.10.5"
+
     jacoco
 }
 
@@ -22,7 +24,6 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.5.0")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.data:spring-data-jpa:3.2.5")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-devtools")
 
